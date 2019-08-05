815b62295eb9068b4c77b59a3e0130026998e362
jenny <jenn7nm@gmail.com>
2019-08-05 11:22:05 -0400

    create config file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jenn:jenny@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SECRET_KEY=os.environ.get('SECRET_KEY')or '123'
+    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    @staticmethod
+    def init_app(app):
+        pass
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass   
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jenn:jenny@localhost/pitch'
+    DEBUG = True
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jenn:jenny@localhost/pitch'
+    pass
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
