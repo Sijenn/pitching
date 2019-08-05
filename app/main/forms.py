327907d120c0177050e17d781473c94744af4f71
jenny <jenn7nm@gmail.com>
2019-08-05 11:20:56 -0400

    create app folder

---
+++ app/main/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
+from wtforms.validators import Required, DataRequired
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    pitch = SelectField('pitch',choices=[('pickup lines','pickup lines'),('interview pitch','interview pitch'),('product pitch','product pitch')])
+    title = StringField('Post title',validators=[DataRequired()])
+    post = TextAreaField('Write your post',validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write your comment',validators=[DataRequired()])
+    submit = SubmitField('submit')
